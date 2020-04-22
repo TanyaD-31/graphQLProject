3bbc3f36d3fde20a00572f6040a52ad21595a2ee
unknown <you@example.com>
2020-04-23 01:04:55 +0300

    change in database

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 import express from "express";
-
 import expressGraphQL from "express-graphql";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import schema from "./graphql/GraphQLSchema";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || "3025";
+const PORT = process.env.PORT || "3000";
 const db = process.env.MONGODB_URL;
 
-mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    console.log("Connected to database");
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(()=>{
+    console.log("Connected to MongoDB");
 }).catch(error => console.log(error));
 
 app.use(
